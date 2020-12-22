75672e44ad72c639538f572b376fbd57a4a12d77
Oguzhan Bas <osan55600@gmail.com>
2020-12-22 17:04:05 +0300

    Login sayfayı yapıldı

--- server.js
+++ server.js
@@ -1,21 +1,94 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server); 
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const passwordHash = require('password-hash');
+const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-accout-key.json');
+const { v4: uuidv4 } = require('uuid');
+const { LocalStorage } = require('node-localstorage');
+const localStorage = new LocalStorage('./scratch');
 const PORT = 3000;
-app.set('view engine', 'ejs');
-app.use(express.static('public/'));
 
-app.get('/',(request,response) => {
-    response.render('chat',{
-        name : 'OGUZHAN',
-    });
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
 });
 
+const db = admin.firestore();
 
-io.on('connection',socket => {
-    
+app.set('view engine', 'ejs');
+app.use(express.static('public/'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', (request, response) => {
+    if (localStorage.getItem('chatUser')) {
+        response.render('chat', {
+            name: 'OGUZHAN',
+        });
+    }
+    else {
+        response.redirect('/login');
+    }
+
+});
+
+app.get('/login', (request, response) => {
+    response.render('login', {
+
+    });
+    console.log(request.method);
+});
+
+app.post('/login', (request, response) => {
+    const { mail, password } = request.body;
+    db.collection("users")
+        .where("mail", "==", mail)
+        .get()
+        .then((querySnapshot) => {
+            if (querySnapshot._size < 1) {
+                console.log("Kullanıcı bulunamadı.");
+            }
+            else {
+                querySnapshot.forEach(function (doc) {
+                    if(passwordHash.verify(password, doc.data().password)){
+
+                        const {name,surname,email} = doc.data();
+
+                        localStorage.setItem('chatUser',{
+                            user_id : doc.id,
+                            name : name,
+                            surname : surname,
+                            email : email
+                        }); 
+                    }
+                    else{
+                        console.log("başarısız");
+                    }         
+                });
+            }
+        }).then(() =>{
+            response.redirect('/');
+        }  );  
+});
+
+io.on('connection', socket => {
+
+    socket.on('message', (message, userId, chatId) => {
+        socket.emit('message', {
+            message,
+            userId,
+            chatId
+        });
+
+        console.log(message, userId, chatId);
+    });
+
 })
 
 
-server.listen(PORT,() => console.log(`http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
